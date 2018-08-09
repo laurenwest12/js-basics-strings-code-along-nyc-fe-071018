fd270a7f7f09d630e71f6b0af1d628f0b9466abe
Lauren West <laurenwest12@gmail.com>
2018-08-09 00:47:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ let currentUser = "user";
 let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
 
 let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+
